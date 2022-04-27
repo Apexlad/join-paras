39a2d3e73fd58c6bee50aaac9cb9ebe15fa005f5
Apexlad <90754052+Apexlad@users.noreply.github.com>
2022-04-27 17:24:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-var inputs = []
-
-function getParagraph1()
-{
-  for (var i = 1; i <=6; i++)
-    {
-      inputs.push(document.getElementById("para1_input_box_" + i).value);
+function getParagraph1(){
+    var inputs =[];
+    for(var i = 1; i <=6 ; i++){
+        inputs.push(document.getElementById("para1_input_box_" + i).value);
     }
-  
-  inputs.join(".");
-  
- document.getElementById("showParagraph1").innerHTML
+    document.getElementById("showParagraph1").innerHTML = inputs.join(". ")
+    
 }
+
+function getParagraph2(){
+    var inputs =[];
+    for(var i = 1; i <=6 ; i++){
+        inputs.push(document.getElementById("para2_input_box_" + i).value);
+    }
+    document.getElementById("showParagraph2").innerHTML = inputs.join(". ")
